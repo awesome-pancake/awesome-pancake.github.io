f8b3419b7c7971ab1613f1a01c56d56134e9402c
awesome-pancake <emmetthoffman@icloud.com>
2026-03-09 11:03:53 -0600

    Made consistent units, added a basic speed input

--- array-assignment/sketch.js
+++ array-assignment/sketch.js
@@ -6,18 +6,21 @@
 // - Learned about classes and methods
 // - Also learned about object methods
 
+// This simulation uses a unit system based on SI:
+// 1 px = 38.891 nm
+// 1 frame = 1 ns
+// 1 AMU = 6.022x10^-26 kg
 const DIAMETER = 10; // Diameter of each particle in pixels
 const MASS = 4; // Mass of each particle in AMU
-const SCALE = 7.4130; // Picometers per pixel, each frame is 1 picosecond
-const K = 1.308*10**-23; // Boltzmann constant in SI units
-let kinetic = 0; // Average kinetic energy in AMU*(pixel/frame)^2
+const K = 0.1; // Boltzmann constant
+let startSpeed = 1;
 
 class Particle {
   constructor(_x, _y){
     this.x = _x;
     this.y = _y;
-    this.dx = random(-1,1);
-    this.dy = random(-1,1);
+    this.dx = random(-startSpeed,startSpeed);
+    this.dy = random(-startSpeed,startSpeed);
   }
 
   speed(){
@@ -40,31 +43,42 @@ let bounds = {
   thickness: 5
 };
 
+let state = {
+  kineticEnergy: 0,
+  pressure: 0,
+  temperature: 0,
+  number: 0,
+  volume: 0,
+};
+
 bounds.inBounds = function(_x, _y, space=0) {
   return _x >= bounds.x + space && _y >= bounds.y + space && _x + space <= bounds.x + bounds.w && _y + space <= bounds.y + bounds.h;
 };
 
 function findTemp(){
   // Finds temperature of gas based on formula KE = 3kT/2
-  return 2.206*10**-21*kinetic/K; // The constant in this formula comes from dimensional analysis
+  state.temperature = state.kineticEnergy*2/3/K;
 }
 
 function findPressure(){
   // Finds pressure based on formula PV = NkT
-  return K*findTemp()/(bounds.w*bounds.h*bounds.l*SCALE**3)/10**33;
+  state.pressure = K*state.number*state.temperature/state.volume;
 }
 
 function update(){
   // Updates the state of each particle and draws them
 
-  kinetic = 0;
+  state.kineticEnergy = 0;
+  state.number = particleArray.length;
+  state.volume = bounds.l * bounds.w * bounds.h;
+
   strokeWeight(0);
   for(let p of particleArray){
     // Draw particle
     fill(p.speedColour());
     circle(p.x, p.y, DIAMETER);
 
-    kinetic += 0.5*MASS*p.speed()**2/particleArray.length;
+    state.kineticEnergy += 0.5*MASS*p.speed()**2/particleArray.length;
 
     // Edge detection
     if(p.x + DIAMETER/2 >= bounds.x + bounds.w || p.x - DIAMETER/2 <= bounds.x){
@@ -78,7 +92,17 @@ function update(){
     p.x += p.dx;
     p.y += p.dy;
   }
+
+  findTemp();
+  findPressure();
 }
+
+/*function mouseWheel(event){
+  // Changes the speed of the spawning gas
+  // Make this better
+  startSpeed += startSpeed >= 0.01 ? 0.01*event.delta : -0.01*event.delta;
+  update();
+}*/
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -90,11 +114,11 @@ function draw() {
 
   fill("black");
   text(`PV = NkT`, 100, 700);
-  text(`N=${particleArray.length}`, 100, 740);
-  text(`V=${round(bounds.w * bounds.h * bounds.l * SCALE**3, 2)} pm^3`, 100, 770);
-  //text(`KE(avg)=${round(kinetic, 2)}`, 100, 800);
-  text(`T=${round(findTemp(),2)}K`, 100, 800);
-  text(`P=${round(findPressure(),2)}P`, 100, 830);
+  text(`N=${state.number}`, 100, 740);
+  text(`V=${round(state.volume*5.88*10**-5, 2)} μm^3`, 100, 770);
+  //text(`KE(avg)=${round(state.kineticEnergy, 2)}`, 100, 800);
+  text(`T=${round(state.temperature,2)} K`, 100, 800);
+  text(`P=${round(state.pressure*2.511*10**6,2)} MPa`, 100, 830);
 
   strokeWeight(bounds.thickness);
   fill("white");
